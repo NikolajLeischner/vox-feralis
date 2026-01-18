4d4d0ebd175add5ffec0c44736d21bb0b995041c
Nikolaj Leischner <jjp@oddbeat.de>
2026-01-18 22:24:57 +0100

    Work in progress.

---
+++ src/_data/meta.js
@@ -0,0 +1,94 @@
+export const url = process.env.URL || 'http://localhost:8080';
+// Extract domain from `url`
+export const band = "Vox Feralis";
+export const domain = new URL(url).hostname;
+export const siteName = band;
+export const siteDescription = 'Eine Thrash-Metal-Band aus Köln';
+export const siteType = 'Band'; // schema
+export const locale = 'de_DE';
+export const lang = 'de';
+export const skipContent = 'Skip to content';
+export const author = {
+  name: band, // author's name. Must be set.
+  avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
+  email: 'webmaster@vox-feralis.de',
+};
+export const creator = {
+  name: band,
+  email: 'webmaster@vox-feralis.de'
+};
+export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
+export const themeColor = '#981a04'; // used in manifest, for example primary color value
+export const themeLight = '#f8f8f8'; // used for meta tag theme-color, if light colors are prefered. best use value set for light bg
+export const themeDark = '#2e2e2e'; // used for meta tag theme-color, if dark colors are prefered. best use value set for dark bg
+export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
+export const opengraph_default_alt = "Visible content: "; // alt text for default meta image"
+export const blog = {
+  // RSS feed
+  name: 'Vox Feralis Blog',
+  description: 'News von Vox Feralis',
+  // feed links are looped over in the head. You may add more to the array.
+  feedLinks: [
+    {
+      title: 'Atom Feed',
+      url: '/feed.xml',
+      type: 'application/atom+xml'
+    },
+    {
+      title: 'JSON Feed',
+      url: '/feed.json',
+      type: 'application/json'
+    }
+  ],
+  // Tags
+  tagSingle: 'Tag',
+  tagPlural: 'Tags',
+  tagMore: 'Mehr tags:',
+  // pagination
+  paginationLabel: 'Blog',
+  paginationPage: 'Page',
+  paginationPrevious: 'Previous',
+  paginationNext: 'Next',
+  paginationNumbers: true
+};
+export const details = {
+  aria: 'section controls',
+  expand: 'expand all',
+  collapse: 'collapse all'
+};
+export const dialog = {
+  close: 'Close',
+  next: 'Next',
+  previous: 'Previous'
+};
+export const navigation = {
+  navLabel: 'Menu',
+  ariaTop: 'Main',
+  ariaBottom: 'Complementary',
+  ariaPlatforms: 'Platforms',
+  drawerNav: false,
+  subMenu: false
+};
+export const themeSwitch = {
+  title: 'Theme',
+  light: 'light',
+  dark: 'dark'
+};
+export const greenweb = {
+  // https://carbontxt.org/
+  disclosures: [],
+  services: []
+};
+export const tests = {
+  pa11y: {
+    // keep customPaths empty if you want to test all pages
+    customPaths: [],
+    globalIgnore: []
+  }
+};
+export const viewRepo = {
+  // this is for the view/edit on github link. The value in the package.json will be pulled in.
+  allow: false,
+  infoText: 'View this page on GitHub'
+};
+export const easteregg = false;
---
+++ src/_data/navigation.js
@@ -0,0 +1,18 @@
+export default {
+  top: [
+    {
+      text: 'About',
+      url: '/about/'
+    },
+    {
+      text: 'Blog',
+      url: '/blog/'
+    }
+  ],
+  bottom: [
+    {
+      text: 'Impressum',
+      url: '/imprint/'
+    }
+  ]
+};
